0c3345170fcf60629486dfe1990a45c7069042b7
Jelmer de Ronde <jelmer.deronde@surfnet.nl>
2018-03-28 15:23:14 +0200

    Cleaning up

--- project.clj
+++ project.clj
@@ -5,12 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [io.pedestal/pedestal.service "0.5.3"]
-
-                 ;; Remove this line and uncomment one of the next lines to
-                 ;; use Immutant or Tomcat instead of Jetty:
                  [io.pedestal/pedestal.jetty "0.5.3"]
-                 ;; [io.pedestal/pedestal.immutant "0.5.3"]
-                 ;; [io.pedestal/pedestal.tomcat "0.5.3"]
 
                  [ch.qos.logback/logback-classic "1.1.8" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.22"]
@@ -21,8 +16,6 @@
                  [cheshire "5.8.0"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
-  ;; If you use HTTP/2 or ALPN, use the java-agent to pull in the correct alpn-boot dependency
-  ;:java-agents [[org.mortbay.jetty.alpn/jetty-alpn-agent "2.0.5"]]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "analyzer.server/run-dev"]}
                    :dependencies [[io.pedestal/pedestal.service-tools "0.5.3"]]}
              :uberjar {:aot [analyzer.server]}}
--- src/analyzer/service.clj
+++ src/analyzer/service.clj
@@ -23,16 +23,11 @@
   [request]
   (let [[in file-name] ((juxt :tempfile :filename)
                         (-> request :params (get "file")))]
-
-  ;; do something with file
-  ;;(io/copy in (io/file OUTPUT_DIR file-name))
     {:status 200
      :headers {"Content-Type" "application/json"}
      :body (json/generate-string (analyze in))}))
 
-;; Defines "/" and "/about" routes with their associated :get handlers.
-;; The interceptors defined after the verb map (e.g., {:get home-page}
-;; apply to / and its children (/about).
+
 (def common-interceptors [(body-params/body-params) http/html-body])
 (def upload-interceptors [(middlewares/multipart-params) (body-params/body-params) http/html-body])
 
@@ -40,57 +35,14 @@
 (def routes #{["/" :get (conj common-interceptors `home-page)]
               ["/upload" :post (conj upload-interceptors `upload)]})
 
-;; Map-based routes
-;(def routes `{"/" {:interceptors [(body-params/body-params) http/html-body]
-;                   :get home-page
-;                   "/about" {:get about-page}}})
-
-;; Terse/Vector-based routes
-;(def routes
-;  `[[["/" {:get home-page}
-;      ^:interceptors [(body-params/body-params) http/html-body]
-;      ["/about" {:get about-page}]]]])
-
-
 ;; Consumed by analyzer.server/create-server
 ;; See http/default-interceptors for additional options you can configure
 (def service {:env :prod
-              ;; You can bring your own non-default interceptors. Make
-              ;; sure you include routing and set it up right for
-              ;; dev-mode. If you do, many other keys for configuring
-              ;; default interceptors will be ignored.
-              ;; ::http/interceptors []
               ::http/routes routes
-
-              ;; Uncomment next line to enable CORS support, add
-              ;; string(s) specifying scheme, host and port for
-              ;; allowed source(s):
-              ;;
-              ;; "http://localhost:8080"
-              ;;
-              ;;::http/allowed-origins ["scheme://host:port"]
-
-              ;; Tune the Secure Headers
-              ;; and specifically the Content Security Policy appropriate to your service/application
-              ;; For more information, see: https://content-security-policy.com/
-              ;;   See also: https://github.com/pedestal/pedestal/issues/499
-              ;;::http/secure-headers {:content-security-policy-settings {:object-src "'none'"
-              ;;                                                          :script-src "'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:"
-              ;;                                                          :frame-ancestors "'none'"}}
-
-              ;; Root for resource interceptor that is available by default.
               ::http/resource-path "/public"
-
-              ;; Either :jetty, :immutant or :tomcat (see comments in project.clj)
-              ;;  This can also be your own chain provider/server-fn -- http://pedestal.io/reference/architecture-overview#_chain_provider
               ::http/type :jetty
-              ;;::http/host "localhost"
               ::http/port 8080
-              ;; Options to pass to the container (Jetty)
               ::http/container-options {:h2c? true
                                         :h2? false
-                                        ;:keystore "test/hp/keystore.jks"
-                                        ;:key-password "password"
-                                        ;:ssl-port 8443
                                         :ssl? false}})
 
