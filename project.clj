599e623ce141d48430b1cbb07963db168f590a91
Jelmer de Ronde <jelmer.deronde@surfnet.nl>
2019-10-03 13:34:58 +0200

    Updates pedestal version

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject analyzer "0.0.1-SNAPSHOT"
+(defproject analyzer "0.0.2"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [io.pedestal/pedestal.service "0.5.3"]
-                 [io.pedestal/pedestal.jetty "0.5.3"]
+                 [io.pedestal/pedestal.service "0.5.7"]
+                 [io.pedestal/pedestal.jetty "0.5.7"]
 
                  [ch.qos.logback/logback-classic "1.1.8" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.22"]
@@ -13,11 +13,11 @@
                  [org.slf4j/log4j-over-slf4j "1.7.22"]
 
                  [com.novemberain/pantomime "2.10.0"]
-                 [cheshire "5.8.0"]]
+                 [cheshire "5.9.0"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "analyzer.server/run-dev"]}
-                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.3"]]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.7"]]}
              :uberjar {:aot [analyzer.server]}}
   :main ^{:skip-aot true} analyzer.server)
 
