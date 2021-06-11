c0f2fd74eabcc7d7455524b62d5e3cf63eca8f16
Jelmer de Ronde <jelmer.deronde@surf.nl>
2021-06-11 10:58:46 +0200

    Add /analyze endpoint

--- src/analyzer/service.clj
+++ src/analyzer/service.clj
@@ -5,25 +5,47 @@
             [io.pedestal.http :as http]
             [io.pedestal.http.body-params :as body-params]
             [io.pedestal.http.ring-middlewares :as middlewares]
-            [ring.util.response :as ring-resp]))
+            [ring.util.response :as ring-resp])
+  (:import [org.apache.commons.validator.routines UrlValidator]))
+
+(defn valid-url?
+  [url-str]
+  (let [default-validator (UrlValidator.)]
+    (.isValid default-validator url-str)))
 
 (defn home-page
   [_]
   (ring-resp/response "ok"))
 
-(defn analyze
-  [file]
+(defn analyze-file-or-url
+  [file-or-url]
   (merge
-    {:mime-type (p/mime-type-of file)}
-    (pextract/parse file)))
+    {:mime-type (p/mime-type-of file-or-url)}
+    (pextract/parse file-or-url)))
 
-(defn upload
+(analyze-file-or-url "https://www.youtube.com/watch?v=13cmHf_kt-Q")
+
+(defn upload-file
   [request]
   (let [[in _] ((juxt :tempfile :filename)
                 (-> request :params (get "file")))]
     {:status 200
      :headers {"Content-Type" "application/json"}
-     :body (json/generate-string (analyze in))}))
+     :body (json/generate-string (analyze-file-or-url in))}))
+
+(defn analyze-url
+  [request]
+  (if-let [url (-> request :json-params :url)]
+    (if (valid-url? url)
+      {:status 200
+       :headers {"Content-Type" "application/json"}
+       :body (json/generate-string (analyze-file-or-url url))}
+      {:status 400
+       :headers {"Content-Type" "text/plain"}
+       :body "The supplied url is not a valid url."})
+    {:status 400
+     :headers {"Content-Type" "text/plain"}
+     :body "Missing url. Make sure you send a json body with a \"url\" key."}))
 
 
 (def common-interceptors [(body-params/body-params) http/html-body])
@@ -31,7 +53,8 @@
 
 ;; Tabular routes
 (def routes #{["/" :get (conj common-interceptors `home-page)]
-              ["/upload" :post (conj upload-interceptors `upload)]})
+              ["/upload" :post (conj upload-interceptors `upload-file)]
+              ["/analyze" :post (conj upload-interceptors `analyze-url)]})
 
 ;; Consumed by analyzer.server/create-server
 ;; See http/default-interceptors for additional options you can configure
